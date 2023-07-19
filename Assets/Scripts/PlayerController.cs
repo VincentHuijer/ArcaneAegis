ef2c6692bbf93157313b9f57b38e0afe2a59273b
Helloguis <94686622+VincentHuijer@users.noreply.github.com>
2023-07-19 15:06:01 +0200

    NPC interaction

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,6 +13,8 @@ public class PlayerController : MonoBehaviour
     private Animator animator;
 
     public LayerMask solidObjectsLayer;
+
+    public LayerMask interactableLayer;
     private void Awake() //Wanneer de script instance is geladen, wordt deze code afgespeeld.
     {
      animator = GetComponent<Animator>();     
@@ -25,8 +27,7 @@ public class PlayerController : MonoBehaviour
             input.x = Input.GetAxisRaw("Horizontal");
             input.y = Input.GetAxisRaw("Vertical");
 
-            Debug.Log("this is input.x" + input.x);
-            Debug.Log("this is input.y" + input.y);
+            
             //if (input.x != 0) input.y = 0; //als ik naar links of rechts ga, dan wil ik dat mn input.y gelijk is aan 0 (stopt schuin lopen)
             if (input != Vector2.zero)
             {
@@ -43,12 +44,31 @@ public class PlayerController : MonoBehaviour
                 }
             }
         }
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Interact();
+        }
     }
 
-    private bool isWalkable(Vector3 targetPos)
+    void Interact()
     {
-        if (Physics2D.OverlapCircle(targetPos, 0.2f, solidObjectsLayer) !=
-            null) //als de positie waar we heen willen lopen zou overlappen met een solid object dan is het lopen false.
+        var facingDir = new Vector3(animator.GetFloat("moveX"), animator.GetFloat("moveY"));
+        var interactPos = transform.position + facingDir; //Welke positie je ook bent. + waar je naar kijkt. = de interact positie om te kunnen interacten met NPCs of objecten.
+
+        Debug.DrawLine(transform.position, interactPos, Color.red, 1f); //Als je de game speelt en klikt met linker muisknop, zie je in de scene een rode streep met je line of sight voor interacties.
+
+
+        var collider = Physics2D.OverlapCircle(interactPos, 0.2f, interactableLayer); //hierin kijken we of we overlappen met de interactpositie en de interactable layer. .
+        if (collider != null) //Als op het einde van de interactPos lijn, er een interactableLayer is, dan doe iets (interacten)
+        {
+            Debug.Log("Hier is iets!");
+        } 
+    }
+
+    private bool isWalkable(Vector3 targetPos) //bekijkt of iets is waarop je kan lopen (background) of dat er een object is. Dan niet.
+    {
+        if (Physics2D.OverlapCircle(targetPos, 0.2f, solidObjectsLayer | interactableLayer) != null) //als de positie waar we heen willen lopen zou overlappen met een solid object dan is het lopen false.
         {
             return false;
         }
