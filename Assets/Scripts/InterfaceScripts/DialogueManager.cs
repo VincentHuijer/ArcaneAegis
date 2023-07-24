88a4f53889be11fd42cff5ae081894ed54a2a006
Helloguis <94686622+VincentHuijer@users.noreply.github.com>
2023-07-24 18:32:34 +0200

    Code cleanup

--- Assets/Scripts/InterfaceScripts/DialogueManager.cs
+++ Assets/Scripts/InterfaceScripts/DialogueManager.cs
@@ -1,1 +1,33 @@
-﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    [SerializeField] GameObject dialogueBox;
+    [SerializeField] Text dialogueText;
+    [SerializeField] int lettersPerSecond;
+
+    public static DialogueManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    public void ShowDialogue(Dialogue dialogue)
+    {
+        dialogueBox.SetActive(true);
+        StartCoroutine(TypeDialogue(dialogue.Lines[0]));
+    }
+
+    public IEnumerator TypeDialogue(string line)
+    {
+        dialogueText.text = ""; //eerst wordt het op 0 gezet
+        foreach (var letter in line.ToCharArray()) //vervolgens voor iedere letter wordt het op het scherm getoond.
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(1f / lettersPerSecond);
+        }
+    }
+}
