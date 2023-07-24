987beb45031cdf68a055aad8719602b82acf109a
Helloguis <94686622+VincentHuijer@users.noreply.github.com>
2023-07-25 00:39:07 +0200

    dialogue af

--- Assets/Scripts/CharacterScripts/GameController.cs
+++ Assets/Scripts/CharacterScripts/GameController.cs
@@ -4,23 +4,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
-public enum GameState {FreeRoam, Dialogue, Battle}
-    public class GameController : MonoBehaviour
+public enum GameState { FreeRoam, Dialogue, Battle } //freeRoam = kunnen rondlopen, Dialogue = in een gesprek, Battle = in een gevecht.
+public class GameController : MonoBehaviour
+{
+    [SerializeField] PlayerController playerController;
+
+    GameState state;
+
+    private void Start() //we moeten wisselen tussen states. We doen dit door de instantie van de dialogue manager voor show en hide.
     {
-        [SerializeField] PlayerController playerController;
-
-        GameState state;
-
-        private void Update()
+        DialogueManager.Instance.onShowDialogue += () =>
         {
-            if (state == GameState.FreeRoam)
+            state = GameState.Dialogue;
+        };
+
+
+        {
+            DialogueManager.Instance.onHideDialogue += () => //als de dialogue gesloten is dan ben je weer vrij om te lopen (freeRoam)
             {
-                playerController.HandleUpdate();
-            }
-            else if (state == GameState.Dialogue){
+                if (state == GameState.Dialogue) 
+                    state = GameState.FreeRoam;
+            };
         }
-        
-        else if (state == GameState.Battle){}
-            
-            }
     }
+    private void Update()
+    {
+        if (state == GameState.FreeRoam)
+        {
+            playerController.HandleUpdate();
+        }
+        else if (state == GameState.Dialogue)
+        {
+            DialogueManager.Instance.HandleUpdate();
+        }
+
+        else if (state == GameState.Battle) { }
+
+    }
+}
--- Assets/Scripts/InterfaceScripts/DialogueManager.cs
+++ Assets/Scripts/InterfaceScripts/DialogueManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,25 +10,56 @@ public class DialogueManager : MonoBehaviour
     [SerializeField] Text dialogueText;
     [SerializeField] int lettersPerSecond;
 
+    public event Action onShowDialogue;
+    public event Action onHideDialogue;
+
     public static DialogueManager Instance { get; private set; }
 
     private void Awake()
     {
         Instance = this;
     }
-    public void ShowDialogue(Dialogue dialogue)
+
+    Dialogue dialogue;
+    int currentLine = 0;
+    bool isTyping; 
+    
+
+    public IEnumerator ShowDialogue(Dialogue dialogue)
     {
+        yield return new WaitForEndOfFrame();
+        onShowDialogue?.Invoke();
+        this.dialogue = dialogue; //hierdoor kan je het dus als een variabele gebruiken.
         dialogueBox.SetActive(true);
         StartCoroutine(TypeDialogue(dialogue.Lines[0]));
     }
 
+    public void HandleUpdate()
+    {
+        if (Input.GetKeyUp(KeyCode.Mouse0) && !isTyping) //if ik mijn dialogue al heb gestart, en ik druk de linkermuisknop (interact knop)
+        {
+            ++currentLine; //voeg ik een nummer aan dit variabele
+            if (currentLine < dialogue.Lines.Count) //als dit minder is dan het totaal aantal lines....
+            {
+                StartCoroutine(TypeDialogue(dialogue.Lines[currentLine])); //...toon de volgende line. tot het einde.
+            }
+            else
+            {
+                dialogueBox.SetActive(false); //als het af is gaat de dialoguebox uit.
+                currentLine = 0; // TODO: Zet de dialogue op 0 zodat je het opnieuw kan afspelen. Kan problemen oplopen als een dialogue niet op 0 moet beginnen.
+                onHideDialogue?.Invoke(); //vertel het systeem om het de verstoppen.
+            }
+        }
+    }
     public IEnumerator TypeDialogue(string line)
     {
+        isTyping = true;
         dialogueText.text = ""; //eerst wordt het op 0 gezet
         foreach (var letter in line.ToCharArray()) //vervolgens voor iedere letter wordt het op het scherm getoond.
         {
             dialogueText.text += letter;
             yield return new WaitForSeconds(1f / lettersPerSecond);
         }
+        isTyping = false;
     }
 }
